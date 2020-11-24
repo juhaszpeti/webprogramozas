37bc1271c33b79c2580e498f78c362eebd69c81a
juhaszpeti <74959611+juhaszpeti@users.noreply.github.com>
2020-11-24 21:32:09 +0100

    1.rész
    Első rész amiben, megvalósítottam nagy vonalakban a kinézetet, a kép galériát feltöltöttem és a bejelentkezést megvalósítottam.

---
+++ 1.rész/includes/config.inc.php
@@ -0,0 +1,39 @@
+<?php
+$ablakcim = array(
+    'cim' => 'Kutyával egy mosolyért alapítvány',
+);
+
+$fejlec = array(
+    'kepforras' => 'logo3.png',
+    'kepalt' => 'logo',
+	'cim' => 'Kutyával egy mosolyért alapítvány',
+	'motto' => ''
+);
+
+$lablec = array(
+    'copyright' => 'Copyright '.date("Y").'.',
+    'ceg' => 'Mini honlap Kft.'
+);
+
+$oldalak = array(
+	'/' => array('fajl' => 'cimlap', 'szoveg' => 'Címlap', 'menun' => array(1,1)),
+    'magunkrol' => array('fajl' => 'magunkrol', 'szoveg' => 'Magunkról', 'menun' => array(1,1)),
+    'kepek' => array('fajl' => 'kepek', 'szoveg' => 'Képek', 'menun' => array(1,1)),
+    'feltolt' => array('fajl' => 'feltolt', 'szoveg' => 'Kép feltöltés', 'menun' => array(0,1)),
+	'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)),
+    'uzenet' => array('fajl' => 'tablazat', 'szoveg' => 'Táblázat', 'menun' => array(1,1)),
+    'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Belépés', 'menun' => array(1,0)),
+    'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0,1)),
+    'belep' => array('fajl' => 'belep', 'szoveg' => '', 'menun' => array(0,0)),
+    'regisztral' => array('fajl' => 'regisztral', 'szoveg' => '', 'menun' => array(0,0))
+    
+);
+$MAPPA = './feltoltottkepek/';
+    $TIPUSOK = array ('.jpg', '.png');
+    $MEDIATIPUSOK = array('image/jpeg', 'image/png');
+    $MAXMERET = 500*1024;
+?>
+<?php
+    
+?>
+
---
+++ 1.rész/index.php
@@ -0,0 +1,17 @@
+<?php
+include('./includes/config.inc.php');
+
+//$keres = current($oldalak);
+$keres = $oldalak['/'];
+if (isset($_GET['oldal'])) {
+	if (isset($oldalak[$_GET['oldal']]) && file_exists("./templates/pages/{$oldalak[$_GET['oldal']]['fajl']}.tpl.php")) {
+		$keres = $oldalak[$_GET['oldal']];
+	}
+	else if (isset($extrak[$_GET['oldal']]) && file_exists("./templates/pages/{$extrak[$_GET['oldal']]['fajl']}.tpl.php")) {
+		$keres = $extrak[$_GET['oldal']];
+	}
+	
+}
+
+include('./templates/index.tpl.php'); 
+?>
